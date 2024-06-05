502fa02c7972628f9e92b038a95e5216a40085a9
MrEdward2546 <xilou254631861@gmail.com>
2024-06-05 11:39:30 +0800

    Create demoscripts.js

---
+++ demoscripts.js
@@ -0,0 +1,13 @@
+var body = $response.body;
+body = {
+    "code": 200,
+    "data":"nye42ub6mKQO9YI84Sp4kpnZn6GaQxa/wM7bKrJwMpa1V3qiHBPk0gPMsBXTI9FZda5Q+4YZgSB4fJJQh58Hcf4IxbuBSiSA9kVG8yCeO01xBUeIeHDqbmGXA7wyvW5O5jmTizK/CuKwe6qjjjE2VZyFtjgPcSx/RomTLfFgr1ARtB3fSRFUtRg812CYqPK7bKJ8hVzXQTngjUvjvpq8t8GYBC6jh3PIc4u7kjjmH98w3S9goCiymrvFxeA66fosA9HphdnrhxqlfxxrQ6NC5w0Lx0RrdDv0o5W47zeg7WGvoMgOooISp78XD/UwoYNuVLQVTEyE3O7qkceAJNWp+Bavz+n9WxSs5eW3/XLpwWe8QpC1M/gWXTo65gKLPfkiOSq0snnU0pYZfROcH+1wbGM4k4jnXApSjdDcjzyShkmQ4rbchV2ZgK0PsHJJnUxD8nld6WQBm3mkQDU7P4f8g7cnqRWUCXAXghsjVA==",
+    "hash": true,
+    "msg": "success",
+    "time": "2024-06-04T13:41:45.208Z",
+    "tip": ""
+}
+
+$done({
+    body: JSON.stringify(body)
+});
